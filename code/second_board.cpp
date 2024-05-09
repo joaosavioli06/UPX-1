6fc6cf1ce93cb9a063d5b2fc0f17b430fb5c79eb
João Gabriel Savioli <joaogabrielsavioli6@gmail.com>
2024-05-09 05:03:54 -0300

    Eliminando anotações desnecessárias

--- code/second_board.cpp
+++ code/second_board.cpp
@@ -14,31 +14,31 @@ byte grauC[8] = {
   B00000
 };
 
-String temperatura; // Variável para armazenar a temperatura recebida
-bool dadosRecebidos = false; // Flag para indicar se todos os dados foram recebidos
+String temperatura; 
+bool dadosRecebidos = false; 
 
 void recebe(int bytes) {
-  if (bytes >= 2) { // Verifica se pelo menos dois bytes foram recebidos
-    int temperaturaInt = Wire.read() << 8; // Lê o byte mais significativo
-    temperaturaInt |= Wire.read(); // Lê o byte menos significativo e combina com o byte mais significativo
-    temperatura = String(temperaturaInt); // Converte a temperatura para uma string
-    dadosRecebidos = true; // Define a flag como true para indicar que todos os dados foram recebidos
+  if (bytes >= 2) { 
+    int temperaturaInt = Wire.read() << 8; 
+    temperaturaInt |= Wire.read(); 
+    temperatura = String(temperaturaInt);
+    dadosRecebidos = true;
   }
 }
 
 void exibirTemperatura() {
   if (dadosRecebidos) {
-    // Exibe a temperatura no LCD
-    lcd.clear(); // Limpa o LCD para remover leituras anteriores
-    lcd.setCursor(0, 0); // Move o cursor para o início da primeira linha
-    lcd.print("Temperatura:"); // Exibe "Temperatura:"
+   
+    lcd.clear(); 
+    lcd.setCursor(0, 0); 
+    lcd.print("Temperatura:"); 
     
     lcd.setCursor(0, 1);
-    lcd.print(temperatura); // Exibe a temperatura recebida
-    lcd.write(byte(0)); // Escreve o caractere personalizado para o símbolo de grau Celsius
-  	lcd.print("C"); // Exibe o símbolo de grau Celsius
+    lcd.print(temperatura);
+    lcd.write(byte(0));
+  	lcd.print("C");
     
-    dadosRecebidos = false; // Reseta a flag para a próxima leitura
+    dadosRecebidos = false;
   }
 }
 
